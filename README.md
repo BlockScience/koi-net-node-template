015ece1cf6dc598c132bcd79a3621f2a0cb5938a
lukvmil <millerluke1364@gmail.com>
2025-12-09 22:13:52 -0500

    updated to koi-net 1.2, removed license from pyproject toml, removed myself as author, updated readme, removed requirements.txt, updated boilerplate code

--- README.md
+++ README.md
@@ -38,10 +38,16 @@ pip install -e .
 ## 6. Set up .env file
 Create a file called `.env` and inside set `PRIV_KEY_PASSWORD=<password_of_your choice>`. This will be used to encrypt your node's private key.
 
-## 7. Run node server
+## 7. Run node
 ```
 python -m koi_net_YOUR_NODE_NAME_node
 ```
 
 # Modifying this Node
 Take a look at the [koi-net repo](https://github.com/BlockScience/koi-net) for documentation about the koi-net package and developing nodes. This template provides the basic structure for a full node setup. You'll likely want to start by modifying `config.py` with the RID types your node deals with, and adding some new knowledge handlers in `handlers.py`.
+
+# Adding a License
+This template doesn't contain any license by default. If you add a `LICENSE` file, make sure to update your `pyproject.toml` with the following line:
+```
+license = {file = "LICENSE"}
+```
--- src/koi_net_CHANGE_NAME_node/config.py
+++ src/koi_net_CHANGE_NAME_node/config.py
@@ -1,17 +1,21 @@
-from pydantic import Field
-from koi_net.config import NodeConfig, KoiNetConfig
-from koi_net.protocol.node import NodeProfile, NodeProvides, NodeType
+from rid_lib.types import KoiNetNode
+from koi_net.config.full_node import (
+    FullNodeConfig, 
+    KoiNetConfig, 
+    ServerConfig, 
+    NodeProfile, 
+    NodeProvides
+)
 
-class MyNodeConfig(NodeConfig):
-    koi_net: KoiNetConfig | None = Field(default_factory = lambda:
-        KoiNetConfig(
-            node_name="my-node-name",   # human readable name for your node
-            node_profile=NodeProfile(
-                node_type=NodeType.FULL,
-                provides=NodeProvides(
-                    event=[],   # RID types of provided events
-                    state=[]    # RID types of provided state
-                )
+
+class MyNodeConfig(FullNodeConfig):
+    koi_net: KoiNetConfig = KoiNetConfig(
+        node_name="my-node-name",   # human readable name for your node
+        node_profile=NodeProfile(
+            provides=NodeProvides(
+                event=[],   # RID types of provided events
+                state=[]    # RID types of provided state
             )
-        )
+        ),
+        rid_types_of_interest=[KoiNetNode] # RID types this node should subscribe to
     )
